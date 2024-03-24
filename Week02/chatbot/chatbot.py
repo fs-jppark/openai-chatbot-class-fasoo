5afd01d29ed6e4b1526aa0174b1f76d2d382b2d1
unknown <jeongmoon@fasoo.com>
2024-03-25 08:38:02 +0900

    comment

--- Week02/chatbot/chatbot.py
+++ Week02/chatbot/chatbot.py
@@ -2,36 +2,43 @@ import streamlit as st
 
 from open_ai_chat import chat
 
+def create_user_message(query):
+    return f"""python에서 {query}"""
 
 def main():
 
-    st.set_page_config()  # 타이틀 정보 입력
-    st.title()  # 챗봇 제목
+    st.set_page_config(page_title="Title is My Chatbot", page_icon="ICON")  # 타이틀 정보 입력
+    st.title("In webbroser... What do you think about?")  # 챗봇 제목
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = []
 
     if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
+        st.session_state.chat_history = [
+            {"role":"assistant", "content":"당신은 챗봇입니다."}
+        ]
 
     if "messages" not in st.session_state:
-        st.session_state["messages"] = [{"role": "assistant",
-                                         "content": ""  # 원하는 인사말을 입력합니다.
-                                         }]
+        st.session_state["messages"] = [
+            {"role":"assistant", "content":"This is ridicuros"},
+            {"role":"assistant", "content":"한글 처리는 어떻게 되나?"},
+            ]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
     if query := st.chat_input("질문을 입력해주세요."):
-        st.session_state.messages.append({"role": "user", "content": query})
+        reqString = create_user_message(query)
+        print(reqString)
+        st.session_state.messages.append({"role": "user", "content": reqString})
 
         with st.chat_message("user"):
-            st.markdown(query)
+            st.markdown(reqString)
 
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
-                response = chat(messages=[{"role":"user", "content": query}])
+                response = chat(messages=[{"role":"user", "content": reqString}])
                 st.markdown(response)
 
         st.session_state.messages.append({"role": "assistant", "content": response})
