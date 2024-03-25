d9c91bdbea11c532074da5c1dc0717d4ff15ec8e
unknown <jeongmoon@fasoo.com>
2024-03-25 22:47:26 +0900

    응답값을 파일에 기록하는 부분 추가

--- Week02/chatbot/chatbot.py
+++ Week02/chatbot/chatbot.py
@@ -1,3 +1,4 @@
+import csv
 import streamlit as st
 
 from open_ai_chat import chat
@@ -7,8 +8,8 @@ def create_user_message(query):
 
 def main():
 
-    st.set_page_config(page_title="Title is My Chatbot", page_icon="ICON")  # 타이틀 정보 입력
-    st.title("In webbroser... What do you think about?")  # 챗봇 제목
+    st.set_page_config(page_title="Phtyon Helper", page_icon="ICON")  # 타이틀 정보 입력
+    st.title("Phtyon 관련 내용 질의하세요")  # 챗봇 제목
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = []
@@ -20,13 +21,20 @@ def main():
 
     if "messages" not in st.session_state:
         st.session_state["messages"] = [
-            {"role":"assistant", "content":"This is ridicuros"},
-            {"role":"assistant", "content":"한글 처리는 어떻게 되나?"},
+            {"role":"assistant", "content":"안녕하세요. 파이썬 쳇봇입니다"}
             ]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
+        
+    with open('my_data.csv', 'r', encoding='utf-8') as csvfile:
+        for line in csvfile:
+            #print(line.strip())    
+            result = line.split(",")[1]
+            st.markdown(result)
+            print(result)
+
 
     if query := st.chat_input("질문을 입력해주세요."):
         reqString = create_user_message(query)
@@ -42,6 +50,12 @@ def main():
                 st.markdown(response)
 
         st.session_state.messages.append({"role": "assistant", "content": response})
+
+        with open('my_data.csv', 'a', newline='', encoding='utf-8') as csvfile:
+            # writer 객체 생성
+            writer = csv.writer(csvfile)
+
+            writer.writerow(['assistant', response])
 
 
 if __name__ == '__main__':
