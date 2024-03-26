2e18270aebc60eb5b16677a95c0d45f26951fc01
server_seeun <ashley1021@fasoo.com>
2024-03-26 09:18:52 +0900

    사내교육 2주차 과제 제출 - 클라우드개발팀 김세은

--- Week02/chatbot/chatbot.py
+++ Week02/chatbot/chatbot.py
@@ -4,19 +4,19 @@ from open_ai_chat import chat
 
 
 def main():
-
-    st.set_page_config()  # 타이틀 정보 입력
-    st.title()  # 챗봇 제목
+    st.set_page_config(page_title="", page_icon=":robot_face:")  # 타이틀 정보 입력
+    st.title("_My :red[Chatbot Demo]_")  # 챗봇 제목
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = []
 
     if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
+        st.session_state.chat_history = [
+            {"role": "assistant", "content": "당신은 KimSeeun 의 챗봇입니다. 당신은 개발자의 직무를 돕는 챗봇입니다. 간결한 어투로 답벼을 주세요."}]
 
     if "messages" not in st.session_state:
         st.session_state["messages"] = [{"role": "assistant",
-                                         "content": ""  # 원하는 인사말을 입력합니다.
+                                         "content": "안녕하세요? 저는 AI 챗봇입니다."  # 원하는 인사말을 입력합니다.
                                          }]
 
     for message in st.session_state.messages:
@@ -31,10 +31,20 @@ def main():
 
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
-                response = chat(messages=[{"role":"user", "content": query}])
+                response = chat(messages=create_messages(st.session_state.chat_history, query))
                 st.markdown(response)
 
         st.session_state.messages.append({"role": "assistant", "content": response})
+        st.session_state.chat_history.append({"role": "user", "content": query})
+        st.session_state.chat_history.append({"role": "assistant", "content": response})
+
+
+def create_messages(old_messages, message):
+    new_messages = []
+    new_messages += old_messages
+    new_messages.append({"role": "user", "content": message})
+    print(new_messages)
+    return new_messages
 
 
 if __name__ == '__main__':
