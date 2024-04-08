13787b2c3fd23af7b67275191075a536d73f08b5
jppark <jppark@fasoo.com>
2024-04-08 19:21:14 +0900

    2주차 코드

---
+++ Week04/chatbot/chatbot.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+from open_ai_chat import chat
+
+
+def main():
+    st.set_page_config(page_title="", page_icon=":robot_face:")  # 타이틀 정보 입력
+    st.title("_My :red[Chatbot Demo]_")  # 챗봇 제목
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = []
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            {"role": "assistant", "content": "당신은 jppark 의 챗봇입니다. 사람들에게 기쁨을 주는 챗봇입니다. 항상 밝고, 명랑한 어조로 대답합니다."}]
+
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant",
+                                         "content": "안녕하세요? 챗봇입니다."  # 원하는 인사말을 입력합니다.
+                                         }]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if query := st.chat_input("질문을 입력해주세요."):
+        st.session_state.messages.append({"role": "user", "content": query})
+
+        with st.chat_message("user"):
+            st.markdown(query)
+
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = chat(messages=create_messages(st.session_state.chat_history, query))
+                st.markdown(response)
+
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.session_state.chat_history.append({"role": "user", "content": query})
+        st.session_state.chat_history.append({"role": "assistant", "content": response})
+
+
+def create_messages(old_messages, message):
+    new_messages = []
+    new_messages += old_messages
+    new_messages.append({"role": "user", "content": message})
+    print(new_messages)
+    return new_messages
+
+
+if __name__ == '__main__':
+    main()
