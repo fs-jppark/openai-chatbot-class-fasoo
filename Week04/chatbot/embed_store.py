5ed4b32c31d454fc448b500ba8e56796d16f3535
serimmm <105839330+serimmm@users.noreply.github.com>
2024-04-16 09:04:47 +0900

    feat: 4주차 과제

--- Week04/chatbot/embed_store.py
+++ Week04/chatbot/embed_store.py
@@ -10,14 +10,27 @@ COLLECTION_NAME = "my_knowledge"
 
 # Chroma 를 이용해 벡터스토어를 만들어주세요.
 class EmbeddingStore:
-    _chroma_client = chromadb.Client()
 
     def __init__(self, api_key=OPEN_AI_KEY, embedding_model=EMBEDDING_MODEL):
         super().__init__()
 
+        self.openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+            api_key=OPEN_AI_KEY,
+            model_name=EMBEDDING_MODEL
+        )
+
+        self._chroma_client = chromadb.Client()
+        self.collection = self._chroma_client.get_or_create_collection(name=COLLECTION_NAME, embedding_function=self.openai_ef)
+        self.embedding_model = EMBEDDING_MODEL
+
     def embedding(self, doc):
-        pass
+        return self.openai_ef(doc)[0]
+
     def query_embedding(self, text, n_results=10):
-        pass
+        query_vector = self.embedding(text)
+        response = self.collection.query(query_embeddings=query_vector, n_results=n_results, include=['documents', 'distances'])
+        return response
+
     def insert_document(self, docs):
-        pass
+        for idx, data in enumerate(docs):
+            self.collection.add(documents=data[0], ids=f"idx-{idx}")
